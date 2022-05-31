91b428d7409e932091bf196b21b84224539a48dd
Valcokoro <valcokoro@yahoo.com>
2022-05-31 12:37:20 -0700

    SideMenu Component

--- src/components/SideMenu.js
+++ src/components/SideMenu.js
@@ -1,13 +1,19 @@
-import React from 'react';
-import dot from '../resources/dot.png'
+import React, {useState} from 'react';
+import dot from '../resources/dot.png';
 import logo from '../resources/logo.png';
 import picture1 from '../resources/picture1.png';
 import overview from '../resources/overview.svg';
 import policy from '../resources/policy.svg';
+import report from '../resources/report.svg';
+import company from '../resources/company.svg';
+import user from '../resources/user.svg';
+import history from '../resources/history.svg';
+
 
 
 
 const SideMenu = () => {
+  
 return (
     <section className='sidebar'>
       <div>
@@ -19,13 +25,8 @@ return (
          <p className='welcome'>Welcome Back To Your Insurance Portal</p>
          <p className='plan'>Your Plan&nbsp;: 
          <span className='freemode'>&nbsp;Free</span></p>
-         <button className='ui button' 
-             style={{width:"190px",
-                     borderRadius:"10px",
-                     marginLeft: "15px",
-                     backgroundColor:"#713BDB",
-                     color:"#FFFBFB"}}
-        >Create New Plan&nbsp;&nbsp;
+         <button variant='contained' className= 'btn'
+        >&nbsp;&nbsp;<b>Create New Plan</b>&nbsp;&nbsp;
          &nbsp;&nbsp;&nbsp;&nbsp;
          <i class="plus icon"></i></button>
        <div className='side-menu'>
@@ -33,6 +34,14 @@ return (
          <a href='' className='overview-page'>Overview</a>
          <img src={policy} alt='policy-icon' className='policy-icon'/>
          <a href='' className='policy-page'>Policy</a>
+         <img src={report} alt='report-icon' className='report-icon'/>
+         <a href='' className='report-page'>Reports</a>
+         <img src={company} alt='company-icon' className='company-icon'/>
+         <a href='' className='company-page'>Company</a>
+         <img src={user} alt='user-icon' className='user-icon'/>
+         <a href='' className='user-page'>User Account</a>
+         <img src={history} alt='history-icon' className='history-icon'/>
+         <a href='' className='history-page'>History</a>
 
         </div>
       </div>
